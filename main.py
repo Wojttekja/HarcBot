e78a079184c3d062efdfde28701856d07fafb514
Wojttekja <79536275+Wojttekja@users.noreply.github.com>
2021-06-29 14:52:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,25 +22,43 @@ async def on_message(message):
     real_message = message.content[1:]
     real_message = real_message.split(" ")
 
-    if real_message[0] == "pyt":
-        print(data[real_message[1]]["pyt"])
-        await message.channel.send(data[real_message[1]]["pyt"])
+    if real_message[0] == "help":
+        print(data["help"])
+        await message.channel.send(data["help"])
+
+    elif real_message[0] == "pyt":
+        try:
+            print(data[real_message[1]]["pyt"])
+            await message.channel.send(data[real_message[1]]["pyt"])
+        except:
+            print("Tylko że takiego zadania nie ma")
+            with open('niema.jpg', 'rb') as f:
+                picture = discord.File(f)
+                await message.channel.send(file=picture)
 
     elif real_message[0] == "odp":
-        if real_message[2] == data[real_message[1]]["odp"]:
-            print("Dobrze")
-            await message.channel.send("dobrze")
+        try:
+            if real_message[2] == data[real_message[1]]["odp"]:
+                print("Dobrze")
+                await message.channel.send("dobrze")
 
-            #próba wysłania kolejenego pytania
-            try:
-                print(data[str(int(real_message[1]) + 1)]["pyt"])
-                await message.channel.send(data[str(int(real_message[1]) + 1)]["pyt"])
-            except:
-                print("To by było na tyle :)")
-                await message.channel.send("To by było na tyle :)")
-        else:
-            print ("Nie tym razem, spróbuj ponownie")
-            await message.channel.send("Nie tym razem, spróbuj ponownie")
-
+                #próba wysłania kolejenego pytania
+                try:
+                    print(data[str(int(real_message[1]) + 1)]["pyt"])
+                    await message.channel.send(data[str(int(real_message[1]) + 1)]["pyt"])
+                except:
+                    print("To by było na tyle :)")
+                    await message.channel.send("To by było na tyle :)")
+            else:
+                print ("Nie tym razem, spróbuj ponownie")
+                await message.channel.send("Nie tym razem, spróbuj ponownie")
+        except:
+            print("Tylko że takiego zadania nie ma")
+            with open('niema.jpg', 'rb') as f:
+                picture = discord.File(f)
+                await message.channel.send(file=picture)
+    else:
+        print("Nie zrozumiałem :(, spróbuj napisać ponownie")
+        await message.channel.send("Nie zrozumiałem :(, spróbuj napisać ponownie")
 
 client.run('')
